99f08c095dbadeea41944d2938a9bb97826b1c0d
Sascha Dungs <sascha.dungs@udo.edu>
2017-06-26 14:27:03 +0200

    Init file for devices

--- e4control/devices/__init__.py
+++ e4control/devices/__init__.py
@@ -1,8 +1,23 @@
 from .HMP4040 import HMP4040
 from .HP4284A import HP4284A
-
+from .ISEG import ISEG
+from .JULABO import JULABO
+from .K196 import K196
+from .K487 import K487
+from .K2000 import K2000
+from .K2410 import K2410
+from .SB22 import SB22
+from TSX3510P import TSX3510P
 
 __all__ = [
     'HMP4040',
     'HP4284A',
+    'ISEG',
+    'JULABO',
+    'K196',
+    'K487',
+    'K2000',
+    'K2410',
+    'SB22',
+    'TSX3510P',
 ]
