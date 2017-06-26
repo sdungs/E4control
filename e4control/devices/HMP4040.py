1e56bb9db14cf8424436055c9f1742236c405ed3
Sascha Dungs <sascha.dungs@udo.edu>
2017-06-26 14:52:28 +0200

    typo

--- e4control/devices/HMP4040.py
+++ e4control/devices/HMP4040.py
@@ -1,60 +1,59 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 from .device import Device
 
+
 class HMP4040(Device):
 
+    def __init__(self, connection_type, host, port):
+        super.__init__(connection_type=connection_type, host=host, port=port)
 
-    def __init__(self,kind,adress,port):
-        super.__init__(kind=kind, adress=adress, port=port)
-
-    def userCmd(self,cmd):
-        print "userCmd: %s" % cmd
+    def userCmd(self, cmd):
+        print('user cmd: %s' % cmd)
         return self.ask(cmd)
 
     def initialize(self):
         pass
 
-    def setVoltageLimit(self,iOutput,fValue):
-        self.write("INST OUT%i"%iOutput)
-        self.write("VOLT:PROT %f"%fValue)
+    def setVoltageLimit(self, iOutput, fValue):
+        self.write('INST OUT%i' % iOutput)
+        self.write('VOLT:PROT %f' % fValue)
 
-    def setVoltage(self,iOutput,fValue):
-        self.write("INST OUT%i"%iOutput)
-        self.write("VOLT %f"%fValue)
+    def setVoltage(self, iOutput, fValue):
+        self.write('INST OUT%i' % iOutput)
+        self.write('VOLT %f' % fValue)
 
-    def setCurrent(self,iOutput,fValue):
-        self.write("INST OUT%i"%iOutput)
-        self.write("CURR %f"%fValue)
+    def setCurrent(self, iOutput, fValue):
+        self.write('INST OUT%i' % iOutput)
+        self.write('CURR %f' % fValue)
 
-    def getVoltage(self,iOutput):
-        self.write("INST OUT%i"%iOutput)
-        return float(self.dv.ask("MEAS:VOLT?"))
+    def getVoltage(self, iOutput):
+        self.write('INST OUT%i' % iOutput)
+        return float(self.dv.ask('MEAS:VOLT?'))
 
-    def getCurrent(self,iOutput):
-        self.write("INST OUT%i"%iOutput)
-        return float(self.dv.ask("MEAS:CURR?"))
+    def getCurrent(self, iOutput):
+        self.write('INST OUT%i' % iOutput)
+        return float(self.dv.ask('MEAS:CURR?'))
 
-    def enableOutput(self,iOutput,bValue):
-        if bValue == True:
-            self.write("INST OUT%i"%iOutput)
-            self.write("OUTP ON")
-        elif bValue == False:
-            self.write("INST OUT%i"%iOutput)
-            self.write("OUTP OFF")
+    def enableOutput(self, iOutput, bValue):
+        if bValue:
+            self.write('INST OUT%i' % iOutput)
+            self.write('OUTP ON')
+        else:
+            self.write('INST OUT%i' % iOutput)
+            self.write('OUTP OFF')
 
-    def getEnableOutput(self,iOutput):
-        self.write("INST OUT%i"%iOutput)
-        return self.dv.ask("OUTP?")
+    def getEnableOutput(self, iOutput):
+        self.write('INST OUT%i' % iOutput)
+        return self.dv.ask('OUTP?')
 
-    def output(self, show = True):
+    def output(self, show=True):
         bPower = []
         fVoltage = []
         fCurrent = []
-        values = []
+        sValues = []
         if show:
-            print("HMP4040:")
+            print('HMP4040:')
         i = 1
         while i <= 4:
             a = self.getEnableOutput(i)
@@ -64,40 +63,40 @@ class HMP4040(Device):
             fVoltage.append(b)
             fCurrent.append(c)
             if show:
-                if a == "1":
-                    print("CH %i:"%i + "\t" + "\033[32m ON \033[0m")
+                if a == '1':
+                    print('CH %i:' % i + '\t' + '\033[32m ON \033[0m')
                 else:
-                    print("CH %i:"%i + "\t" + "\033[31m OFF \033[0m")
-                print("Voltage = %0.1fV"%b + "\t" + "Current = %0.3fA"%c)
-            values.append(str(a))
-            values.append(str(b))
-            values.append(str(c))
+                    print('CH %i:' % i + '\t' + '\033[31m OFF \033[0m')
+                print('Voltage = %0.1fV' % b + '\t' + 'Current = %0.3fA' % c)
+            sValues.append(str(a))
+            sValues.append(str(b))
+            sValues.append(str(c))
             i += 1
-        header = ["CH1","U1[V]","I1[A]","CH2","U2[V]","I2[A]","CH3","U3[V]","I3[A]","CH4","U4[V]","I4[A]"]
-        return([header,values])
+        sHeader = ['CH1', 'U1[V]', 'I1[A]', 'CH2', 'U2[V]', 'I2[A]', 'CH3', 'U3[V]', 'I3[A]', 'CH4', 'U4[V]', 'I4[A]']
+        return([sHeader, sValues])
 
     def interaction(self):
-        ch = raw_input("Choose channel! \n")
-        while ch != "1" and ch != "2" and ch != "3" and ch != "4":
-             ch = raw_input("Possible Channels: 1,2,3 or 4! \n")
-        channel = int(ch)
-        print("1: enable Output")
-        print("2: set Voltage")
-        print("3: set Current")
-        x = raw_input("Number? \n")
-        while x != "1" and x != "2" and x != "3":
-             x = raw_input("Possible Inputs: 1,2 or 3! \n")
-        if x == "1":
-            bO = raw_input("Please enter ON or OFF! \n")
-            if bO == "ON" or bO == "on":
-                self.enableOutput(channel,True)
-            elif bO == "OFF" or bO == "off":
-                self.enableOutput(channel,False)
+        sChannel = raw_input('Choose channel! \n')
+        while sChannel != '1' and sChannel != '2' and sChannel != '3' and sChannel != '4':
+            sChannel = raw_input('Possible Channels: 1,2,3 or 4! \n')
+        iChannel = int(sChannel)
+        print('1: enable Output')
+        print('2: set Voltage')
+        print('3: set Current')
+        x = raw_input('Number? \n')
+        while x != '1' and x != '2' and x != '3':
+            x = raw_input('Possible Inputs: 1,2 or 3! \n')
+        if x == '1':
+            bO = raw_input('Please enter ON or OFF! \n')
+            if bO == 'ON' or bO == 'on':
+                self.enableOutput(iChannel, True)
+            elif bO == 'OFF' or bO == 'off':
+                self.enableOutput(iChannel, False)
             else:
                 pass
-        elif x == "2":
-            fV = raw_input("Please enter new Voltage in V for CH %i\n"%channel)
-            self.setVoltage(channel,float(fV))
-        elif x == "3":
-            fI = raw_input("Please enter new Current in A for CH %i\n"%channel)
-            self.setCurrent(channel,float(fI))
+        elif x == '2':
+            sVoltage = raw_input('Please enter new Voltage in V for CH %i\n' % iChannel)
+            self.setVoltage(iChannel, float(sVoltage))
+        elif x == '3':
+            sCurrent = raw_input('Please enter new Current in A for CH %i\n' % iChannel)
+            self.setCurrent(iChannel, float(sCurrent))
