b67a1859d261502a45fd2a192a5725f6341a6dbf
Sascha Dungs <sascha.dungs@cern.ch>
2017-11-03 09:31:22 +0100

    step back

--- e4control/devices/SB22.py
+++ e4control/devices/SB22.py
@@ -7,21 +7,21 @@ class SB22(Device):
     T_set = None
     H_set = None
     Power = None
-    D_2 = None
-    D_3 = None
-    D_4 = None
-    D_5 = None
-    D_6 = None
-    D_7 = None
-    D_8 = '0'
-    D_9 = '0'
-    D_10 = '0'
-    D_11 = '0'
-    D_12 = '0'
-    D_13 = '0'
-    D_14 = '0'
-    D_15 = '0'
-    D_16 = '0'
+    D2 = None
+    D3 = None
+    D4 = None
+    D5 = None
+    D6 = None
+    D7 = None
+    D8 = '0'
+    D9 = '0'
+    D10 = '0'
+    D11 = '0'
+    D12 = '0'
+    D13 = '0'
+    D14 = '0'
+    D15 = '0'
+    D16 = '0'
 
     def __init__(self, connection_type, host, port):
         super(SB22, self).__init__(connection_type=connection_type, host=host, port=port)
@@ -56,7 +56,7 @@ class SB22(Device):
         return result
 
     def updateChanges(self):
-        line = chr(02)+'1T%sF%sR%s%s%s%s%s%s%s000000000' % (self.T_set, self.H_set, self.Power, self.D_2, self.D_3, self.D_4, self.D_5, self.D_6, self.D_7)
+        line = chr(02)+'1T%sF%sR%s%s%s%s%s%s%s000000000' % (self.T_set, self.H_set, self.Power, self.D2, self.D3, self.D4, self.D5, self.D6, self.D7)
         pn = self.generateChecksum(line)
         cmd = '%s%s' % (line, pn)+chr(03)
         self.write(cmd)
@@ -79,12 +79,12 @@ class SB22(Device):
             p = s.split('$')[1]
         line = p[p.find('R')+1:p.find('R')+8]
         self.Power = line[0]
-        self.D_2 = line[1]
-        self.D_3 = line[2]
-        self.D_4 = line[3]
-        self.D_5 = line[4]
-        self.D_6 = line[5]
-        self.D_7 = line[6]
+        self.D2 = line[1]
+        self.D3 = line[2]
+        self.D4 = line[3]
+        self.D5 = line[4]
+        self.D6 = line[5]
+        self.D7 = line[6]
         self.T_set = p[p.find('T')+1:p.find('F')]
         self.H_set = p[p.find('F')+1:p.find('R')]
 
@@ -131,15 +131,15 @@ class SB22(Device):
 
     def setOperationMode(self, sMode):
         if (sMode == 'climate'):
-            self.D_2 = '0'
+            self.D2 = '0'
         elif (sMode == 'normal'):
-            self.D_2 = '1'
+            self.D2 = '1'
         else:
             print('Unknown mode: %s' % sMode)
         self.updateChanges()
 
     def getOperationMode(self):
-        if (self.D_2 == '0'):
+        if (self.D2 == '0'):
             sMode = 'climate'
         else:
             sMode = 'normal'
@@ -147,33 +147,33 @@ class SB22(Device):
 
     def enableProtectionSystem(self, bEnable):
         if bEnable:
-            self.D_3 = '1'
+            self.D3 = '1'
         else:
-            self.D_3 = '0'
+            self.D3 = '0'
 
     def enableDewPointExtension(self, bEnable):
         if bEnable:
-            self.D_4 = '1'
+            self.D4 = '1'
         else:
-            self.D_4 = '0'
+            self.D4 = '0'
 
     def enableCapacitiveHumidity(self, bEnable):
         if bEnable:
-            self.D_5 = '1'
+            self.D5 = '1'
         else:
-            self.D_5 = '0'
+            self.D5 = '0'
 
     def enableEngerySavingMode(self, bEnable):
         if bEnable:
-            self.D_6 = '1'
+            self.D6 = '1'
         else:
-            self.D_6 = '0'
+            self.D6 = '0'
 
     def enableAdjustableAirFanSpeed(self, bEnable):
         if bEnable:
-            self.D_7 = '1'
+            self.D7 = '1'
         else:
-            self.D_7 = '0'
+            self.D7 = '0'
 
     def output(self,  show=True):
         sMode = self.getOperationMode()
@@ -185,7 +185,7 @@ class SB22(Device):
                 print('Power: \033[32m ON \033[0m')
             else:
                 print('Power: \033[31m OFF \033[0m')
-        if (self.D_2 == '0'):
+        if (self.D2 == '0'):
             fHset = self.getSetHumidity()
             fHac = self.getHumidity()
             if show:
@@ -222,7 +222,7 @@ class SB22(Device):
             fT = raw_input('Please enter new Temperature in °C \n')
             self.setTemperature(float(fT))
         elif x == '4':
-            if (self.D_2 == '0'):
+            if (self.D2 == '0'):
                 fH = raw_input('Please enter new Humidity \n')
                 self.setHumidity(float(fH))
             else:
