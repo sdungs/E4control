36431449378af67a918d85da3f988c7cb5e9b804
JoeLoe <jonas.loenker@tu-dortmund.de>
2018-06-27 09:03:33 +0200

    Playing around with IOError for READ2 in the SHT-Server-Script

--- e4control/devices/SHT_Server.py
+++ e4control/devices/SHT_Server.py
@@ -1,11 +1,17 @@
 import socket
 from sht_sensor import Sht
+from subprocess import call
+import signal
 #declare sensor on pins 14 and 4
 sens1 = Sht(14,4)
 #declare sensor on pins 8 and 11
 sens2 = Sht(8,11)
 #setup server/socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+def handler(signum, frame):
+    raise IOError('SHT not answering.')
+
 try:
     s.bind(("",50000))
     #s.bind("129.217.167.177", 50000))
@@ -19,12 +25,15 @@ try:
             print(data)
             data = data.decode('utf-8')
             if data == 'READ2':
+                    signal.signal(signal.SIGALRM, handler)
+                    signal.alarm(3)
                     temp1 = sens1.read_t()
                     hum1 =  round(sens1._read_rh(temp1),2)
                     temp2 = sens2.read_t()
                     hum2 =  round(sens2._read_rh(temp2),2)
                     answer = str('%.2f' % temp1) + ',' +  ',' + str('%.2f' % hum1) + ',' + str('%.2f' % temp2)  + str('%.2f' % hum2)
                     komm.send(answer.encode())
+                    signal.alarm(0)
                     print(answer)
             elif data == 'READ':
                     temp1 = sens1.read_t()
@@ -36,6 +45,7 @@ try:
                 answer = 'command unknown'
                 komm.send(answer.encode())
         break
+
 finally:
         print('\n Socket closed')
         s.close()
