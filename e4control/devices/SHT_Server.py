fcfcd2f2fcc5fbabbc0be2126c6559bdb00f61b1
Andreas Gisen <andreas.gisen@tu-dortmund.de>
2019-01-09 16:02:02 +0100

    Overhaul of SHT75.

--- e4control/devices/SHT_Server.py
+++ e4control/devices/SHT_Server.py
@@ -7,7 +7,7 @@ sens1 = Sht(14,4)
 #declare sensor on pins 8 and 11
 sens2 = Sht(8,11)
 #Possibility to add third sensor for Elenas purpose:
-sens3 =  Sht(20,26, voltage='5V') 
+sens3 = Sht(20,26, voltage='5V')
 
 #setup server/socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -22,44 +22,53 @@ try:
 
     while True:
         komm, addr = s.accept()
-        connected = True
-        while connected:
+        while True:
             data = komm.recv(1024)
             print(data)
             data = data.decode('utf-8')
-            if data == 'READ2':
-                   # signal.signal(signal.SIGALRM, handler)
-                    #signal.alarm(1)
-                    temp1 = sens1.read_t()
+            if data == 'READ':
+                try:
+                    temp1 = round(sens1.read_t(),2)
                     hum1 =  round(sens1._read_rh(temp1),2)
-                    try:
-                        temp2 = sens2.read_t()
-                        hum2 =  round(sens2._read_rh(temp2),2)
-                        #signal.alarm(0)
-                    except:
-                        temp2 = temp1
-                        hum2 = hum1
-                    answer = str('%.2f' % temp1) + ',' +  str('%.2f' % hum1) + ',' + str('%.2f' % temp2)  + ',' +  str('%.2f' % hum2)
-                    komm.send(answer.encode())
-                    print(answer)
-            elif data == 'READ':
-                    temp1 = sens1.read_t()
-                    hum1 =  round(sens1._read_rh(temp1),2)
-                    answer = str('%.2f' % temp1) + ',' +  str('%.2f' % hum1)
-                    komm.send(answer.encode())
-                    print(answer)
+                except:
+                    temp1 = 'nan'
+                    hum1 = 'nan'
+                    print('Exception while reading sensor 1.')
+                try:
+                    temp2 = round(sens2.read_t(),2)
+                    hum2 =  round(sens2._read_rh(temp2),2)
+                except:
+                    temp2 = 'nan'
+                    hum2 = 'nan'
+                    print('Exception while reading sensor 2.')
+                answer = '{},{},{},{}'.format(temp1,hum1,temp2,hum2)
+                komm.send(answer.encode())
+                print(answer)
+            elif data == 'READ1':
+                temp = round(sens1.read_t(),2)
+                hum =  round(sens1._read_rh(temp),2)
+                answer = '{},{}'.format(temp,hum)
+                komm.send(answer.encode())
+                print(answer)
+            elif data == 'READ2':
+                temp = round(sens2.read_t(),2)
+                hum =  round(sens2._read_rh(temp),2)
+                answer = '{},{}'.format(temp,hum)
+                komm.send(answer.encode())
+                print(answer)
             elif data == 'READ3':
-            	    temp = []
-            	    hum = []
-            	    temp.append(round(sens1.read_t(),2))
-            	    temp.append(round(sens2.read_t(),2))
-            	    temp.append(round(sens3.read_t(),2))
-            	    hum.append(round(sens1.read_rh(),2))
-            	    hum.append(round(sens2.read_rh(),2))
-            	    hum.append(round(sens3.read_rh(),2))
-            	    answer = str('%.2f' % temp[0]) + ',' +  str('%.2f' % hum[0]) + ',' + str('%.2f' % temp[1])  + ',' +  str('%.2f' % hum[1]) + ',' + str('%.2f' % temp[2])  + ',' +  str('%.2f' % hum[2])
-            	    komm.send(answer.encode())
-            	    print(answer)
+                temp = []
+                hum = []
+                temp.append(round(sens1.read_t(),2))
+                temp.append(round(sens2.read_t(),2))
+                temp.append(round(sens3.read_t(),2))
+                hum.append(round(sens1.read_rh(),2))
+                hum.append(round(sens2.read_rh(),2))
+                hum.append(round(sens3.read_rh(),2))
+                answer = '{},{},{},{},{},{}'.format(temp[0],hum[0],temp[1],hum[1],temp[2],hum[2])
+                komm.send(answer.encode())
+                print(answer)
+
             elif data == 'CLOSE':
                 break
 
