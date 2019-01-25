1d393a6506401e1b604cc17cabc801f2be6b6081
jloenker <32542935+jloenker@users.noreply.github.com>
2019-01-25 15:00:35 +0100

    Changes in HMP4040, setup.py and deleted CintMeas.py (#1)
    * script Removed CintMeas.py
    
    * Update setup.py
    
    * Fixed typo in HMP4040 and improved fromatting of strings
    
    * Updated setup.py by deleting entry point for CintMeas

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from setuptools import setup
 setup(
     name='e4control',
     version='0.0.3',
-    author='Sascha Dungs, Jonas Lönker',
-    author_email='sascha.dungs@tu-dortmund.de, jonas.loenker@tu-dortmund.de',
+    author='Sascha Dungs, Andreas Gisen, Jonas Lönker', 
+    author_email='sascha.dungs@tu-dortmund.de, andreas.gisen@tu-dortmund.de, jonas.loenker@tu-dortmund.de',
     packages=[
         'e4control',
         'e4control.devices',
@@ -23,7 +23,6 @@ setup(
         'console_scripts': [
             'e4control_measure_IV = e4control.scripts.IVmeas:main',
             'e4control_measure_CV = e4control.scripts.CVmeas:main',
-            'e4control_measure_Cint = e4control.scripts.CintMeas:main',
             'e4control_measure_It = e4control.scripts.Itmeas:main',
             'e4control_dcs = e4control.scripts.dcs:main',
         ]
