57aca6a78c19af44f46b8742cc9edb84f5b3e1c4
Andreas Gisen <andreas.gisen@tu-dortmund.de>
2019-03-07 15:49:16 +0100

    SHT75s can now also be used during CV measurements for T and H measurements at the same time.

--- e4control/scripts/CVmeas.py
+++ e4control/scripts/CVmeas.py
@@ -59,6 +59,15 @@ def main():
     if devices['I']:
         Ameter, Ameter_channel = sh.device_connection(devices['I'])
 
+    # check if SHT75 is used for T and H
+    for idx_h,d_h in enumerate(devices['H']):
+        if d_h[0]=='SHT75':
+            for idx_t,d_t in enumerate(devices['T']):
+                if d_t[0]=='SHT75':
+                    if d_h[1]==d_t[1] and d_h[2]==d_t[2] and d_h[3]==d_t[3]: 
+                        humidity[idx_h]=temperature[idx_t]
+                        print('Linked H{} with T{}.'.format(idx_h+1,idx_t+1))
+
     # set active source
     d = source[0]
     ch = source_channel[0]
