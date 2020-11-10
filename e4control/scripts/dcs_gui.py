f5c78df1414d49388c1578dbf1af76022ada35d3
Sebastian-Pape <SePa@gmx.de>
2020-11-10 17:45:43 +0100

    Continue work on dcs gui

--- e4control/scripts/dcs_gui.py
+++ e4control/scripts/dcs_gui.py
@@ -6,6 +6,7 @@ import argparse
 import time
 import termios, tty
 import PySimpleGUI as sg
+import numpy as np
 from e4control import __version__ as version
 
 # This is to include a matplotlib figure in a Tkinter canvas
@@ -37,8 +38,184 @@ class pressedKeyThread(Thread):
         print('"{}" has been pressed.'.format(self.pressed_key))
 
 
+def get_timestamp(starttime):
+    timestamp = (time.time() - starttime) / 60
+    h, s = divmod(timestamp, 1)
+    return int(np.round(h, 0)) , int(np.round(s * 60, 0))
+
+
+def change_channel(device_dict):
+    number_channels = device_dict['channel']
+    layout_device_channel = [
+            [sg.Text('Which Channel?')]
+    ]
+    button_names_channel = []
+    for i in np.linspace(1, number_channels, number_channels):
+        layout_device_channel.append([sg.Button(f'CH{int(i)}')])
+        button_names_channel.append(f'CH{int(i)}')
+
+    window_channel = sg.Window('Select Channel', layout_device_channel)
+
+    while True:
+        event_channel, value_channel = window_channel.read()
+        if event_channel == sg.WIN_CLOSED:
+            window_channel.close()
+            abort('Error: No Channel assigned.')
+            break
+        if event_channel in button_names_channel:
+            iChannel = button_names_channel.index(event_channel)
+            window_channel.close()
+            break
+    return iChannel + 1
+
+
+def toogle_output(device):
+    if device.getOutput() == '1':
+        device.rampVoltage(0)
+        device.setOutput(False)
+        return 'Off'
+    else:
+        device.setOutput(True)
+        return 'On'
+
+
 def control_window(devices):
     starttime = time.time()
+    layout = [
+            [sg.Text('CONTROL CENTER', size=(14,3))],
+            [sg.Text('Runtime: '), sg.Text(size=(2,1), key=f'timestamp_min'), sg.Text('min'), sg.Text(size=(2,1), key=f'timestamp_sec'), sg.Text('s')],
+    ]
+
+    device_counter = 0
+    device_names = []
+    for d in devices:
+        device_name = d.__class__.__name__
+        device_names.append(f'{device_name}')
+        layout.append([sg.Text(f'\n{device_name}', size=(len(device_name), 2))])
+        blockPrint()
+        header, values = d.output()
+        for h, v in zip(header, values):
+            layout.append([sg.Text(f'{h}:\t'), sg.Text(size=(12,1), key=f'{h}{device_counter}')])
+        device_counter += 1
+    layout.append([sg.Button('Change'), sg.Button('Quit')])
+
+    window = sg.Window(f'E4control v{version}: Device control script', layout)
+
+    while True:
+        event, value = window.read(timeout=0)
+        if event == sg.WIN_CLOSED or event == 'Quit':
+            window.close()
+            abort()
+
+
+        if event == 'Change':
+            layout_change = []
+            device_counter = 0
+            button_names_change = []
+            for d in device_names:
+                if device_names.count(d) > 1:
+                    button_names_change.append(f'{d} {device_counter}')
+                    layout_change.append([sg.Button(f'{d} {device_counter}')])
+                    device_counter += 1
+                else:
+                    button_names_change.append(f'{d}')
+                    layout_change.append([sg.Button(f'{d}')])
+
+
+
+            layout_change.append([sg.Button('Back')])
+
+            window_change = sg.Window('List of active devices', layout_change)
+            while True:
+                event_change, value_change = window_change.read()
+                if event_change == sg.WIN_CLOSED or event_change == 'Back':
+                    window_change.close()
+                    break
+                if event_change in button_names_change:
+                    iChannel = 0 # default value, if a device does not has any channels
+                    device_change = devices[button_names_change.index(event_change)]
+                    device_interaction_dict = device_change.interaction(gui=True)
+                    d_i_d_keys = list(device_interaction_dict.keys())
+                    button_names_interaction = []
+                    layout_device_interaction = []
+                    if 'pass' in d_i_d_keys:
+                        layout_device_interaction.append([sg.Text('Nothing to do.')])
+
+                    if 'channel' in d_i_d_keys:
+                        iChannel = change_channel(device_interaction_dict)
+
+                        layout_device_interaction.append(
+                                [sg.Text(f'Current Channel: ', size=(18, 2)), sg.Text(size=(3, 2), key='CH'), sg.Button('Switch Channel')]
+                        )
+                        button_names_interaction.append('Switch Channel')
+
+                    if 'toogleOutput' in d_i_d_keys:
+                        layout_device_interaction.append(
+                                [sg.Text(f'Output: ', size=(18, 2)), sg.Text(size=(3, 2), key='Output_status'), sg.Button('Toogle Output')]
+                        )
+                        # layout_device_interaction.append([sg.Button('Toogle Output')])
+                        button_names_interaction.append('Toogle Output')
+
+                    if 'setVoltage' in d_i_d_keys:
+                        layout_device_interaction.append(
+                                [sg.Text(f'Voltage: ', size=(18, 2)), sg.Text(size=(10, 2), key='voltage_status'), sg.Text('V', size=(2,2)), sg.Button('Change Voltage')]
+                        )
+                        # layout_device_interaction.append([sg.Button('Change Voltage')])
+                        button_names_interaction.append('Toogle Output')
+
+
+
+                    layout_device_interaction.append([sg.Button('Back')])
+
+                    window_device_interaction = sg.Window(f'{device_change.__class__.__name__}', layout_device_interaction)
+
+                    while True:
+                        event_interaction, values_interaction = window_device_interaction.read(timeout=0)
+                        if event_interaction == sg.WIN_CLOSED or event_interaction == 'Back':
+                            window_device_interaction.close()
+                            break
+                        if 'channel' in d_i_d_keys:
+                            window_device_interaction['CH'].update(iChannel)
+                        if event_interaction == 'Switch Channel':
+                            iChannel = change_channel(device_interaction_dict)
+                        if 'toogleOutput' in d_i_d_keys:
+                            if device_change.getOutput() == '1':
+                                output_status = 'On'
+                            else:
+                                output_status = 'Off'
+                            window_device_interaction['Output_status'].update(output_status)
+                        if event_interaction == 'Toogle Output':
+                            output_status = toogle_output(device_change)
+
+            # {
+            # 'pass': ,
+            # 'channel': ,
+            # 'enableOutput': ,
+            # 'toogleOutput'
+            # 'setVoltage': ,
+            # 'setCurrent': ,
+            # 'serCurrentLimit': ,
+            # 'setTemperature': ,
+            # 'getStatus': ,
+            # }
+            # x = int(input('Choose the number of a Device:'))
+            # if (x-1) in range(len(config_devices)):
+            #     devices[x-1].interaction()
+            # else:
+            #     continue
+        device_counter = 0
+        for d in devices:
+            header, values = d.output()
+            for h, v in zip(header, values):
+                window[f'{h}{device_counter}'].update(v)
+            device_counter += 1
+
+        h, s = get_timestamp(starttime)
+        window['timestamp_min'].update(h)
+        window['timestamp_sec'].update(s)
+        time.sleep(1)
+    enablePrint()
+
 
     while True:
         key_thread = pressedKeyThread()
@@ -62,13 +239,13 @@ def control_window(devices):
                     h, v = d.output()
                     values += v
                     print('-----------------------------------------------------')
-            if args.logfile:
-                sh.write_line(fw, values)
+            # if args.logfile:
+                # sh.write_line(fw, values)
             time.sleep(1)
             print('press c (=CHANGE PARAMETER) or q (=QUIT)')
             print('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx')
             if key_thread.is_alive():
-                time.sleep(10)
+                time.sleep(5)
             print('')
 
         key_thread.join()
@@ -138,11 +315,17 @@ def welcome_dcs_gui(config):
     pass
 
 
-def abort():
-    layout = [
-            [sg.Text(f'Aborted!')],
-            [sg.Button('Quit')]
-    ]
+def abort(err_msg=False):
+    if err_msg:
+        layout = [
+                [sg.Text(f'{err_msg}')],
+                [sg.Button('Quit')]
+        ]
+    else:
+        layout = [
+                [sg.Text(f'Aborted!')],
+                [sg.Button('Quit')]
+        ]
     window = sg.Window(f'E4control v{version}: Device control script', layout)
     while True:
         event, values = window.read()
@@ -151,9 +334,18 @@ def abort():
             sys.exit()
     pass
 
+# Disable printing
+def blockPrint():
+    sys.stdout = open(os.devnull, 'w')
+    # pass
 
-def print(string):
-    sys.stdout.write(string+'\r\n')
+# Restore printing
+def enablePrint():
+    sys.stdout = sys.__stdout__
+
+
+# def print(string):
+#     sys.stdout.write(string+'\r\n')
 
 def main():
     args = parser.parse_args()
