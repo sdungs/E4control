aebe744b4af0c8de7ae4ec2e3f9cc26d2ff3ad8b
Sebastian-Pape <SePa@gmx.de>
2020-11-12 13:08:51 +0100

    new Version containing dcs_gui

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='e4control',
-    version='1.7.0',
+    version='1.7.1',
     author='Sascha Dungs, Andreas Gisen, Jonas Lönker, Sebastian Pape',
     author_email='sascha.dungs@tu-dortmund.de, andreas.gisen@tu-dortmund.de, jonas.loenker@tu-dortmund.de, sebastian2.pape@tu-dortmund.de',
     packages=[
@@ -24,7 +24,8 @@ setup(
             'e4control_measure_IV = e4control.scripts.IVmeas:main',
             'e4control_measure_CV = e4control.scripts.CVmeas:main',
             'e4control_measure_It = e4control.scripts.Itmeas:main',
-            'e4control_dcs = e4control.scripts.dcs_gui:main',
+            'e4control_dcs = e4control.scripts.dcs:main',
+            'e4control_dcs_gui = e4control.scripts.dcs_gui:main',
         ]
     }
 )
